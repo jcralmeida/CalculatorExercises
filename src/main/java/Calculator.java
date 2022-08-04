661548a525091545395be5f22dc61a98d9cb965b
Jose Almeida <almeidajcr90@gmail.com>
2022-08-04 11:30:37 -0300

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,86 @@
+public class Calculator implements Operators {
+  @Override
+  public long sum(long number1, long number2) {
+    return 0;
+  }
+
+  @Override
+  public long sum(long number1, long number2, long number3) {
+    return 0;
+  }
+
+  @Override
+  public long sumNaturalNumbers(long number1, long number2) {
+    return 0;
+  }
+
+  @Override
+  public double sum(double number1, double number2) {
+    return 0;
+  }
+
+  @Override
+  public double sum(double number1, double number2, double number3) {
+    return 0;
+  }
+
+  @Override
+  public long subtraction(long number1, long number2) {
+    return 0;
+  }
+
+  @Override
+  public long subtraction(long number1, long number2, long number3) {
+    return 0;
+  }
+
+  @Override
+  public long subtractionNaturalNumbers(long number1, long number2) {
+    return 0;
+  }
+
+  @Override
+  public double subtraction(double number1, double number2) {
+    return 0;
+  }
+
+  @Override
+  public double subtraction(double number1, double number2, double number3) {
+    return 0;
+  }
+
+  @Override
+  public double multiplication(double number1, double number2) {
+    return 0;
+  }
+
+  @Override
+  public double multiplication(double number1, double number2, double number3) {
+    return 0;
+  }
+
+  @Override
+  public long division(long number1, long number2) {
+    return 0;
+  }
+
+  @Override
+  public long division(long number1, long number2, long number3) {
+    return 0;
+  }
+
+  @Override
+  public long divisionNaturalNumbers(long number1, long number2) {
+    return 0;
+  }
+
+  @Override
+  public double division(double number1, double number2) {
+    return 0;
+  }
+
+  @Override
+  public double division(double number1, double number2, double number3) {
+    return 0;
+  }
+}
---
+++ src/main/java/Operators.java
@@ -0,0 +1,25 @@
+public interface Operators {
+
+  long sum(long number1, long number2);
+  long sum(long number1, long number2, long number3);
+  long sumNaturalNumbers(long number1, long number2);
+  double sum(double number1, double number2);
+  double sum(double number1, double number2, double number3);
+
+  long subtraction(long number1, long number2);
+  long subtraction(long number1, long number2, long number3);
+  long subtractionNaturalNumbers(long number1, long number2);
+  double subtraction(double number1, double number2);
+  double subtraction(double number1, double number2, double number3);
+
+  double multiplication(double number1, double number2);
+  double multiplication(double number1, double number2, double number3);
+
+  long division(long number1, long number2);
+  long division(long number1, long number2, long number3);
+  long divisionNaturalNumbers(long number1, long number2);
+  double division(double number1, double number2);
+  double division(double number1, double number2, double number3);
+
+
+}
